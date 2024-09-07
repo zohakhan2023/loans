97ddc3ee138f73e070b0560443e472a7c88f189e
zohakhan2018 <zoha_khan@yahoo.com>
2024-09-07 16:35:30 -0400

    9/7 commit

---
+++ src/test/java/com/zoha/loans/LoansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoha.loans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
